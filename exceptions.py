848191d1df35246f8682e5a90cb8de7a1810c25a
Chad Estioco <chadestioco@gmail.com>
2014-01-24 19:13:34 +0800

    Added exceptions module.

---
+++ exceptions.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+class GamePrivelegeError(Exception):
+    """
+    This error is thrown if a game character attempts an action for which the
+    character has no sufficient privilege.
+    """
+
+    def __init__(self, character):
+        self.character = character
+
+    def __str__(self):
+        return repr(self.character) + " does not have sufficient privileges for that action."
