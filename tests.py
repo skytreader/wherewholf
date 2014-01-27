1928e7b43e36ae37f98a86895a7bf5459c287642
Chad Estioco <chadestioco@gmail.com>
2014-01-27 11:30:29 +0800

    More unit tests for the character privileges.

--- tests.py
+++ tests.py
@@ -9,6 +9,18 @@ class CharacterPrivileges(unittest.TestCase):
     def test_privileges(self):
         # Werewolf tests
         self.assertTrue(NotedVillagers.can_kill(NotedVillagers.WEREWOLF))
+        self.assertFalse(NotedVillagers.can_check(NotedVillagers.WEREWOLF))
+        self.assertFalse(NotedVillagers.can_guard(NotedVillagers.WEREWOLF))
+        
+        # Seer tests
+        self.assertFalse(NotedVillagers.can_kill(NotedVillagers.SEER))
+        self.assertTrue(NotedVillagers.can_check(NotedVillagers.SEER))
+        self.assertFalse(NotedVillagers.can_guard(NotedVillagers.SEER))
+        
+        #Witch tests
+        self.assertTrue(NotedVillagers.can_kill(NotedVillagers.WITCH))
+        self.assertFalse(NotedVillagers.can_check(NotedVillagers.WITCH))
+        self.assertTrue(NotedVillagers.can_guard(NotedVillagers.WITCH))
 
 if __name__ == "__main__":
     unittest.main()
