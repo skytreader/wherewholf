85deb71e14ac28db1e07a060dd861a1fae4eec01
Chad Estioco <chadestioco@gmail.com>
2019-11-22 09:26:10 +0800

    Add test to ensure that dead players don't partake in village decisions.

--- src/tests/game_characters_tests.py
+++ src/tests/game_characters_tests.py
@@ -1,6 +1,25 @@
 import unittest
 
-from ..game_characters import Player, SanitizedPlayer, Villager, Werewolf
+from ..game_characters import Player, SanitizedPlayer, Villager, Werewolf, WholeGameHive
+from typing import Sequence
+
+
+class InspectablePlayer(Player):
+
+    def __init__(
+        self,
+        name: str,
+        role: "GameCharacter",
+        aggression: float=0.3,
+        suggestibility: float=0.4,
+        persuasiveness: float=0.5
+    ):
+        super().__init__(name, role, aggression, suggestibility, persuasiveness)
+        self.was_asked_for_daytime = False
+
+    def day_consensus(self, players: Sequence[SanitizedPlayer]) -> SanitizedPlayer:
+        self.was_asked_for_daytime = True
+        return super().day_consensus(players)
 
 
 class PlayerTest(unittest.TestCase):
@@ -20,3 +39,26 @@ class PlayerTest(unittest.TestCase):
         for _ in range(100):
             lynch: SanitizedPlayer = self.me.daytime_behavior(self.sanitized)
             self.assertFalse(SanitizedPlayer.is_the_same_player(self.me, lynch))
+
+
+class WholeGameHiveTest(unittest.TestCase):
+
+    def setUp(self):
+        self.me = InspectablePlayer("Chad", Villager())
+        self.players: Set[Player] = set()
+        self.players.add(Player("Christine", Werewolf()))
+        self.players.add(Player("Shara", Werewolf()))
+        self.players.add(self.me)
+        self.players.add(Player("JE", Villager()))
+        self.players.add(Player("Gab", Villager()))
+
+        self.whole_game_hive = WholeGameHive()
+        self.whole_game_hive.add_players(self.players)
+
+    def test_player_death(self):
+        # Betrayed by my so-called "friends", let's say
+        self.whole_game_hive.notify_player_death(self.me)
+        sanitized = [SanitizedPlayer.sanitize(p) for p in self.players]
+        for _ in range(100):
+            self.whole_game_hive.day_consensus(sanitized)
+            self.assertFalse(self.me.was_asked_for_daytime)
